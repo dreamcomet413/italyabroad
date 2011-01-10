c6f0f9993dbe3846645b2836de6da6091d4ecdb6
LeGreenSolutions <legreensolutions@gmail.com>
2011-01-10 17:04:17 +0530

    chat modified

--- app/controllers/site/chat_controller.rb
+++ app/controllers/site/chat_controller.rb
@@ -4,6 +4,10 @@ class Site::ChatController < ApplicationController
 
   def index
     session[:juggernaut_channels] = ["chat_channel"]
+    @support_user = Setting.find(:first).support
+
+    @support_status = Juggernaut.show_client(@support_user) ? true : false
+
     if current_user != :false
        session[:chat_user_name] = current_user.login
      end
@@ -12,23 +16,27 @@ class Site::ChatController < ApplicationController
 
   def send_data
 #    i+=1
+    @support_user = Setting.find(:first).support
+
     time = "<span class='time'>[#{(Time.now).strftime '%d%b-%T'}]</span>"
 
 
-    if session[:chat_user_name] == "admin"
+    if session[:chat_user_name] == @support_user
 
     if !params[:reply_to].blank?
-      render :juggernaut  => {:type => :send_to_clients_on_channel, :channel => session[:juggernaut_channels], :client_ids => [params[:reply_to], session[:chat_user_name]] }  do |page|
+      render :juggernaut  => {:type => :send_to_clients_on_channel, :channel => session[:juggernaut_channels], :client_ids => [params[:reply_to], @support_user] }  do |page|
         p request.remote_ip
         page.insert_html :bottom, 'chat_data', "<li>#{time}<span class='support_name'>#{session[:chat_user_name]}:</span> <span class='support_chat_data'>#{h params[:chat_input]}</span></li>"
         page.call 'scrollup',"chat_data"
+        page.call 'window_to_front'
       end
     else
 
-      render :juggernaut  => {:type => :send_to_client_on_channel, :channel => session[:juggernaut_channels], :client_id => "admin" }  do |page|
+      render :juggernaut  => {:type => :send_to_client_on_channel, :channel => session[:juggernaut_channels], :client_id => @support_user}  do |page|
         p request.remote_ip
         page.insert_html :bottom, 'chat_data', "<li>#{time}<span class='support_name'>#{session[:chat_user_name]}:</span> <span class='support_chat_error'>No Chat User Selected !</span></li>"
         page.call 'scrollup',"chat_data"
+
       end
     end
 
@@ -38,19 +46,19 @@ class Site::ChatController < ApplicationController
     else
 
 
-      render :juggernaut  => {:type => :send_to_client_on_channel, :channel => session[:juggernaut_channels], :client_id => "admin" }  do |page|
+      render :juggernaut  => {:type => :send_to_client_on_channel, :channel => session[:juggernaut_channels], :client_id => @support_user }  do |page|
         reply_link = "<a class=\"reply_link\" href=\"#\" onclick=\"$('reply_to').value='#{session[:chat_user_name]}'; $('label_reply').innerHTML='Reply to :: #{session[:chat_user_name]}'; $(this).remove(); return false;\">Reply</a>"
         p request.remote_ip
         page.insert_html :bottom, 'chat_data', "<li>#{time}<span class='chat_user_name'>#{session[:chat_user_name]}:</span> <span class='chat_user_data'>#{h params[:chat_input]}</span> #{reply_link}</li>"
         page.call 'scrollup',"chat_data"
-
+        page.call 'window_to_front'
 
       end
 
       render :juggernaut  => {:type => :send_to_client_on_channel, :channel => session[:juggernaut_channels], :client_id => session[:chat_user_name] }  do |page|
-        reply_link = "<a href=\"#\" onclick=\"$('reply_to').value='#{session[:chat_user_name]}';return false;\">Reply</a>"
         page.insert_html :bottom, 'chat_data', "<li>#{time}<span class='chat_user_name'>#{session[:chat_user_name]}:</span> <span class='chat_user_data'>#{h params[:chat_input]}</span> </li>"
          page.call 'scrollup',"chat_data"
+         page.call 'window_to_front'
 
       end
     end
@@ -60,8 +68,8 @@ class Site::ChatController < ApplicationController
 
   def create_chat_user
     p params[:chat_user]
-   if params[:chat_user].to_s.downcase == "admin"
-     flash[:notice] = "Chat username \"admin\" not allowed "
+   if params[:chat_user].to_s.downcase == @support_user.to_s.downcase
+     flash[:notice] = "Chat username #{@support_user} not allowed "
    else
     session[:chat_user_name] = params[:chat_user]
    end
