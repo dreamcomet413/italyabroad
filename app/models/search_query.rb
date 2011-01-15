3bb76f46537a9d3aa92bff1e937a9e431943b98d
LeGreenSolutions <legreensolutions@gmail.com>
2011-01-15 17:44:31 +0530

    product link style and Popular Searches modified

--- app/models/search_query.rb
+++ app/models/search_query.rb
@@ -1,7 +1,8 @@
 class SearchQuery < ActiveRecord::Base
   validates_presence_of :query
-  
+
   def self.popular_searches(limit=7)
     self.find(:all, :select => "COUNT(query) as popularity, query", :group => "query", :order => "popularity desc", :limit => limit).sort_by { rand() }
   end
 end
+
