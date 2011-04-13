55fc7dccf83327d5e5b8ff8d4b3bf09ce77b5943
sujith <sujithsurendran@yahoo.com>
2011-04-13 09:26:02 +0530

    testing image size001

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -112,25 +112,7 @@ module ApplicationHelper
 
   def show_image_tag(image_type, image, format = :jpg, image_alt = "italyabroad.com")
     if image_type && image && format
-      return image_tag(image_url(image_type, image, format), :alt => image_alt)
-    elsif image_type
-      return image_tag("no_images/#{image_type}.jpg", :size => "100x120", :alt => image_alt)
-    else
-      return image_tag("no_images/noimage.png", :alt => image_alt)
-    end
-  end
-
-  def show_image_tag_product(image_type, image, format = :jpg, image_alt = "italyabroad.com", image_size = "-1")
-    if image_type && image && format
-
-      if image_size = "-1"
-          return image_tag(image_url(image_type, image, format), :alt => image_alt)
-      else
-          #return image_tag(image_url(image_type, image, format), {:alt => image_alt, :size => "230x250"})
-          return image_tag(image_url(image_type, image, format), {:alt => image_alt, :size => "23x25", :title => "testingggg"})
-      end
-
-
+      return image_tag(image_url(image_type, image, format), {:alt => image_alt, :title => image_alt})
     elsif image_type
       return image_tag("no_images/#{image_type}.jpg", :size => "100x120", :alt => image_alt)
     else
