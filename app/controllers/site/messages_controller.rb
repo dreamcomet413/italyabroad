44dfa9ee4774d5f153541968080799f3f2a31ee7
Indu R Nair <induvallikunnath@gmail.com>
2011-04-19 18:53:20 +0530

    Option for sending reply to message added in two pages

--- app/controllers/site/messages_controller.rb
+++ app/controllers/site/messages_controller.rb
@@ -13,5 +13,26 @@ class Site::MessagesController < ApplicationController
       redirect_to customer_path(@message.user_id)
    end
   end
+
+  def send_reply
+      respond_to do |format|
+      format.html{ render :update do |page|
+        @message = Message.find(params[:msg_id])
+        page.replace_html 'reply' + params[:msg_id],:partial=>'new_message',:object=>@message
+      end
+      }
+    end
+  end
+
+  def send_message
+    @message = Message.new(:name=>params[:name],:user_id=>params[:user_id],:send_by_id=>params[:send_by_id])
+    if @message.save
+      redirect_to  account_path
+    else
+      flash[:notice] = @message.show_errors
+      redirect_to customer_path(params[:user_id])
+    end
+  end
+
 end
 
