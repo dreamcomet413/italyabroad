1115926e5f78b5ffeaaee8a1be912962365860ba
Indu R Nair <induvallikunnath@gmail.com>
2011-07-14 16:17:07 +0530

    Invoice printing modified

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -52,7 +52,10 @@ end
 
   def print_invoice
     @order = Order.find(params[:id])
-    render :layout => "print"
+    send_data(generate_pdf(@order), :filename => "Invoice of Order# #{@order.id}.pdf", :type => "application/pdf")
+  #  render :layout => "print"
+
+
  #    make_and_send_pdf("/admin/orders/#{@order.id}/print_invoice", "Italyabroad_Invoice_#{@order.id}.pdf")
   end
 
@@ -73,6 +76,81 @@ end
       flash[:notice] = 'The items are shipped'
       redirect_to admin_orders_path
    end
+  end
+
+  def generate_pdf(order)
+
+    Prawn::Document.new do |pdf|
+
+
+     # for order_item in order.order_items
+      #  if (product = Product.find_by_name(order_item.name)) && product.is_wine?
+          pdf.image  "#{RAILS_ROOT}/public/images/pdf_header.jpg",:width => 550, :height => 108
+          pdf.bounding_box([0, 510], :width => 300, :height => 100) do
+            pdf.text 'Sold To' + "\n" +
+             order.user.name + " " + order.user.surname  + "\n" +
+              order.user.address  + "\n" +
+              order.user.address_2 + "\n" +
+              order.user.city  + "\n"
+          end
+           if order.different_shipping_address
+              pdf.bounding_box([200, 510], :width => 300, :height => 100) do
+                pdf.text order.ship_name + "\n" +
+                order.ship_address  + "\n" +
+                order.ship_address_2 + "\n" +
+                order.ship_city  + "\n"+
+                order.ship_cap + "\n" +
+                order.ship_country + "\n"
+              end
+          else
+              pdf.bounding_box([200, 510], :width => 300, :height => 100) do
+             pdf.text order.user.name + " " + order.user.surname  + "\n" +
+              order.user.address  + "\n" +
+              order.user.address_2 + "\n" +
+              order.user.city  + "\n"
+              order.user.cap + "\n" +
+              order.user.country + "\n"
+            end
+          end
+        pdf.text "Order Number: #{ @order.id}"  + "\n"
+		    pdf.text "Order Date:  #{order.created_at.strftime('%d/%m/%Y')}"  + "\n"
+		    pdf.text "Invoice Date:  #{Time.now.strftime('%d/%m/%Y')}"
+         items = order.order_items.map do |item|
+          [
+          item.product_code ,
+          item.quantity,
+          item.name,
+          "£" + item.vat.to_s,
+           item.rate,
+          "£" + item.price.to_s,
+          "£" + item.total.to_s
+        ]
+        end
+        pdf.move_down 10
+        pdf.table items
+
+
+      #  end
+     # end
+     pdf.move_down 10
+     pdf.text "Sub Total: £#{ @order.sub_total}"  + "\n"
+     if @order.sub_total < Setting.order_delivery_amount
+      pdf.text "Sub Total: #{ order.delivery_name} : £#{order.delivery_price} \n"
+    end
+    unless order.cupon_code.blank?
+      pdf.text "Coupon: #{ order.cupon_price}"  + "\n"
+    end
+    if order.gift_option && order.gift_option.price > 0
+      pdf.text "Wrapping option: #{order.gift_option.name} : #{order.gift_option.price} \n"
+    end
+    total =  order.total - (order.points_used * Setting.find(:first).points_to_pound)
+      pdf.text "Total: £#{total} \n"
+    pdf.move_down 70
+    #pdf.image "#{RAILS_ROOT}/public/images/pdf_tasting_footer.png",:position => :center
+   pdf.text "<font size='8'>Don't forget to write your review for the produces purchased.
+			Let our users know what you think about our produces and if you are the first to write the review you will also receive £5 discount on your next purchase.</font>", :inline_format => true
+
+    end.render
   end
 
 end
--- app/controllers/site/orders_controller.rb
+++ app/controllers/site/orders_controller.rb
@@ -126,7 +126,7 @@ class Site::OrdersController < ApplicationController
     puts params[:id]
     @order = Order.find(params[:id])
     @tasting_notes = true
-    send_data(generate_pdf(@order), :filename => "Tasting-Notes.pdf", :type => "application/pdf")
+    send_data(generate_pdf(@order), :filename => "Invoice of Order# #{@order.id}.pdf", :type => "application/pdf")
   end
 
   def show_order_details
@@ -192,19 +192,75 @@ class Site::OrdersController < ApplicationController
 
   def generate_pdf(order)
 
-    Prawn::Document.new do
-      for order_item in order.order_items
-        if (product = Product.find_by_name(order_item.name)) && product.is_wine?
-          image  "#{RAILS_ROOT}/public/images/pdf_tasting_header.png",:width => 550, :height => 108
-          move_down 70
-          text product.name,:align => :center
-          text product.vintage, :align => :center
-          text product.description.gsub("\n","<br />")
-          move_down 40
-          image "#{RAILS_ROOT}/public/images/pdf_tasting_footer.png",:position => :center
+    Prawn::Document.new do |pdf|
 
+
+     # for order_item in order.order_items
+      #  if (product = Product.find_by_name(order_item.name)) && product.is_wine?
+          pdf.image  "#{RAILS_ROOT}/public/images/pdf_tasting_header.png",:width => 550, :height => 108
+          pdf.bounding_box([0, 510], :width => 300, :height => 100) do
+            pdt.text 'Sold To \n' +
+            order.user.name + " " + order.user.surname  + "\n" +
+              order.user.address  + "\n" +
+              order.user.address_2 + "\n" +
+              order.user.city  + "\n"
+          end
+           if order.different_shipping_address
+              pdf.bounding_box([200, 510], :width => 300, :height => 100) do
+              pdf.text order.ship_name + "\n" +
+              order.ship_address  + "\n" +
+              order.ship_address_2 + "\n" +
+              order.ship_city  + "\n"+
+              order.ship_cap + "\n" +
+              order.ship_country + "\n"
+          else
+              pdf.bounding_box([200, 510], :width => 300, :height => 100) do
+             pdf.text order.user.name + " " + order.user.surname  + "\n" +
+              order.user.address  + "\n" +
+              order.user.address_2 + "\n" +
+              order.user.city  + "\n"
+              order.user.cap + "\n" +
+              order.user.country + "\n"
+          end
+          end
+        pdf.text "Order Number: #{ @order.id}"  + "\n"
+		    pdf.text "Order Date:  #{order.created_at.strftime('%d/%m/%Y')}"  + "\n"
+		    pdf.text "Invoice Date:  #{Time.now.strftime('%d/%m/%Y')}"
+         items = order.order_items.map do |item|
+          [
+          item.product_code ,
+          item.quantity,
+          item.name,
+          "£" + item.vat.to_s,
+           item.rate,
+          "£" + item.price.to_s,
+          "£" + item.total.to_s
+        ]
         end
-      end
+        pdf.move_down 10
+        pdf.table items
+
+
+      #  end
+     # end
+     pdf.move_down 10
+     pdf.text "Sub Total: £#{ @order.sub_total}"  + "\n"
+     if @order.sub_total < Setting.order_delivery_amount
+      pdf.text "Sub Total: #{ order.delivery_name} : £#{order.delivery_price} \n"
+    end
+    unless order.cupon_code.blank?
+      pdf.text "Coupon: #{ order.cupon_price}"  + "\n"
+    end
+    if order.gift_option && order.gift_option.price > 0
+      pdf.text "Wrapping option: #{order.gift_option.name} : #{order.gift_option.price} \n"
+    end
+    total =  order.total - (order.points_used * Setting.find(:first).points_to_pound)
+      pdf.text "Total: £#{total} \n"
+    pdf.move_down 70
+    #pdf.image "#{RAILS_ROOT}/public/images/pdf_tasting_footer.png",:position => :center
+   pdf.text "<font size='8'>Don't forget to write your review for the produces purchased.
+			Let our users know what you think about our produces and if you are the first to write the review you will also receive £5 discount on your next purchase.</font>", :inline_format => true
+
     end.render
   end
 end
