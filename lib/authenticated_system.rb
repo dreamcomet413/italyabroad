7c111f91703b350c4047f575d0fbfc708579f81b
Indu R Nair <induvallikunnath@gmail.com>
2011-08-18 10:46:17 +0530

    color of price changed, login msg for a chef changed

--- lib/authenticated_system.rb
+++ lib/authenticated_system.rb
@@ -37,7 +37,7 @@ module AuthenticatedSystem
        username, passwd = get_auth_data
       self.current_user ||= User.authenticate(username, passwd) if username && passwd
     (logged_in? && chef?) || (User.count == 0) ? true : access_denied('site')
-    flash[:notice] = 'You have to login as a chef to add new recipe'
+    flash[:notice] = 'You must be logged in to submit recipes'
     end
 
       def chef?
