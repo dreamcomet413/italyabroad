9318ee17238e521bdaa4c3c705f7c4f0c5c23633
sujith <sujithsurendran@yahoo.com>
2011-08-20 12:37:23 +0530

    producers show image resize removed crop trua and defined new image size

--- app/controllers/site/images_controller.rb
+++ app/controllers/site/images_controller.rb
@@ -16,6 +16,8 @@ class Site::ImagesController < ApplicationController
             #image.resize '80x150' if image_type == :wine_category
             #image.resize '80x150', :crop => true if image_type == :wine_category
             image.resize '80x150', :crop => true if image_type == :wine_category
+            image.resize '80x150' if image_type == :wine_category_producers_page
+
 
             # added by indu on august 2001, according to mail July 29, 2011, 12:44 PM subject - RE: Important - need your help
             image.resize '120x150 ' if image_type == :food_category
@@ -93,10 +95,10 @@ class Site::ImagesController < ApplicationController
   end
 
   def available_image_types
-    %w(category category_thumb home_image home_image_thumb product product_display product_wine product_food avatar_thumb avatar_thumb_small producer_thumb
+    %w(category category_thumb home_image home_image_thumb product product_display product_wine product_food avatar_thumb avatar_thumb_small producer_thumb wine_category_producers_page
        product_hamper product_event product_thumb product_rec restaurant restaurant_thumb restaurant_thumb_site product_show
        recipe recipe_thumb post post_thumb review review_thumb news_letters_header news_letters_thumb news_letters_week
-       news_letters_product news_letters_banner news_letters_other product_wine_tour blog_view product_thumb_cart product_thumb_carta grape_thumb region_thumb_small region_thumb about_thumb region_card wine_category community_producer_thumb wine_tour_category product_hamper_big food_category hamper_category food_sub_category hamper_sub_category wine_sub_category)
+       news_letters_product news_letters_banner news_letters_other product_wine_tour blog_view product_thumb_cart product_thumb_carta grape_thumb region_thumb_small region_thumb about_thumb region_card wine_category community_producer_thumb wine_tour_category product_hamper_big food_category hamper_category food_sub_category hamper_sub_category wine_sub_category  )
   end
 end
 
