0b5fd8465847a1c34e11163e3fec4c00cbfc5502
Indu R Nair <induvallikunnath@gmail.com>
2011-09-19 13:18:58 +0530

    prepartion time and difficulty changed to link in search/recipes, style of price changed in wine club, wine tour and events page

--- app/models/search.rb
+++ app/models/search.rb
@@ -49,7 +49,7 @@ class Search
     conditions = []
     conditions << "recipes.name LIKE '%#{text}%'"
     conditions << "active = #{true}"
-    conditions << "preparation_time #{@preparation_time}" unless @preparation_time.blank?
+    conditions << "preparation_time = #{@preparation_time}" unless @preparation_time.blank?
     conditions << "recipe_type_id = #{@recipe_type}" unless @recipe_type.blank?
     conditions << "recipe_level_id = #{@difficulty}" unless @difficulty.blank?
     conditions << "user_id = #{@user_id}" unless @user_id.blank?
