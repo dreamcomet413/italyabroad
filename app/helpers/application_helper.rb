77fea7e0be48b510316b72366546630888384399
sujith <sujithsurendran@yahoo.com>
2011-09-29 10:46:28 +0530

    image correction

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -178,25 +178,18 @@ module ApplicationHelper
 
 
   def show_grape_image(grape)
-    return image_tag(image_url(:grape_original, grape.image, :jpg)) if grape.image
-    return image_tag("grape_default.png")
+    return image_tag(image_url(:grape_original, grape.image, :jpg), :width => "300px") if grape.image
+    return image_tag("grape_default.png", :width => "300px")
   end
 
   def show_region_image(region)
-    if region.image #,:size => "100x120"
-      return "<div style='height:284;width:367';overflow:hidden;>" + image_tag(image_url(:region_original, region.image, :jpg), :height => "284" ) + "</div>"
-    else
-      return "<div style='height:284;width:367';overflow:hidden;>" + image_tag("region-home-image.jpg",:height => "284")+ "</div>"
-    end
-
-   # return image_tag("region_default.png",:size => "100x120")
+    return image_tag(image_url(:region_original, region.image, :jpg), :width => "300px") if region.image
+    return image_tag("region-home-image.jpg", :width => "300px")
   end
 
    def show_producer_image(producer)
-    return image_tag(image_url(:producer_thumb, producer.image, :jpg)) if producer.image
-   # return image_tag("region_default.png",:size => "100x120")
-   #return image_tag("producer_default.png",:size => "100x120")
-   return image_tag("default.jpg",:size => "100x120")
+    return image_tag(image_url(:producer_thumb, producer.image, :jpg), :width => "300px") if producer.image
+    return image_tag("default.jpg",:width => "300px")
   end
 
   def will_paginate_(object)
