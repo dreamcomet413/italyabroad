549f10558e57a9c03bfc17aab03790a65c44aa81
sujith <sujithsurendran@yahoo.com>
2011-10-15 12:42:04 +0530

    find_wines alignment

--- app/controllers/site/images_controller.rb
+++ app/controllers/site/images_controller.rb
@@ -46,6 +46,10 @@ class Site::ImagesController < ApplicationController
             image.resize '100x75', :crop => true if image_type == :category_thumb
             image.resize '200x200', :crop => true if image_type == :product
             image.resize '800x600' if image_type == :product_display
+
+            image.resize '250' if image_type == :card   #product_show
+
+
             image.resize '60x128' if image_type == :product_wine
             #image.resize '60x128' if image_type == :product_food
             #image.resize '80x150', :crop => true if image_type == :product_food
@@ -61,6 +65,8 @@ class Site::ImagesController < ApplicationController
 
             image.resize '100x100' if image_type == :product_rec
             image.resize '237x180' if image_type == :product_wine_tour
+            image.resize '315' if image_type == :product_wine_tour_left_images
+
             image.resize '237x180' if image_type == :wine_tour_category
             image.resize '300x300' if image_type == :product_show
             image.resize '480x450' if image_type == :recipe
@@ -108,7 +114,7 @@ class Site::ImagesController < ApplicationController
     %w(category category_thumb home_image home_image_thumb product product_display product_wine product_food avatar_thumb avatar_thumb_small producer_thumb wine_category_producers_page
        product_hamper product_event product_thumb product_rec restaurant restaurant_thumb restaurant_thumb_site product_show
        recipe recipe_thumb post post_thumb review review_thumb news_letters_header news_letters_thumb news_letters_week
-       news_letters_product news_letters_banner news_letters_other product_wine_tour blog_view product_thumb_cart product_thumb_carta grape_thumb region_thumb_small region_thumb about_thumb region_card wine_category community_producer_thumb wine_tour_category product_hamper_big food_category hamper_category food_sub_category hamper_sub_category wine_sub_category  )
+       news_letters_product news_letters_banner news_letters_other product_wine_tour blog_view product_thumb_cart product_thumb_carta grape_thumb region_thumb_small region_thumb about_thumb region_card wine_category community_producer_thumb wine_tour_category product_hamper_big food_category hamper_category food_sub_category hamper_sub_category wine_sub_category card product_wine_tour_left_images)
   end
 end
 
