96be108dbe474121b967af036657c3959ec2af5b
sujith <sujithsurendran@yahoo.com>
2012-05-03 12:28:28 +0530

    layout changes

--- app/controllers/site/images_controller.rb
+++ app/controllers/site/images_controller.rb
@@ -85,6 +85,8 @@ class Site::ImagesController < ApplicationController
             image.resize '100x75' if image_type == :review_thumb
 
             image.resize '287x200' if image_type == :blog_view
+            image.resize '723x284',:crop=>true if image_type == :blog_banner
+
             image.resize '300x300',:crop=>true if image_type == :producer_thumb
             image.resize '300x300',:crop=>true if image_type == :region_thumb
             image.resize '100x100',:crop=>true if image_type == :region_thumb_small
@@ -116,7 +118,7 @@ class Site::ImagesController < ApplicationController
     %w(category category_thumb home_image home_image_thumb product product_display product_wine product_food avatar_thumb avatar_thumb_small producer_thumb wine_category_producers_page
        product_hamper product_event product_thumb product_rec restaurant restaurant_thumb restaurant_thumb_site product_show
        recipe recipe_thumb post post_thumb review review_thumb news_letters_header news_letters_thumb news_letters_week
-       news_letters_product news_letters_banner news_letters_other product_wine_tour blog_view product_thumb_cart product_thumb_carta grape_thumb region_thumb_small region_thumb about_thumb region_card wine_category community_producer_thumb wine_tour_category product_hamper_big food_category hamper_category food_sub_category hamper_sub_category wine_sub_category card product_wine_tour_left_images)
+       news_letters_product news_letters_banner news_letters_other product_wine_tour blog_view product_thumb_cart product_thumb_carta grape_thumb region_thumb_small region_thumb about_thumb region_card wine_category community_producer_thumb wine_tour_category product_hamper_big food_category hamper_category food_sub_category hamper_sub_category wine_sub_category card product_wine_tour_left_images blog_banner)
   end
 end
 
