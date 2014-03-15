22c63dd0a90ffabc55e96b1dba020fa0971a9f19
Neeraj Kumar <neeraj.kumar@gmail.com>
2014-03-15 13:45:13 +0530

    removing redundant code from migration files

--- db/migrate/20140312131135_create_admin_wine_sizes.rb
+++ db/migrate/20140312131135_create_admin_wine_sizes.rb
@@ -1,6 +1,5 @@
 class CreateAdminWineSizes < ActiveRecord::Migration
   def self.up
-    drop_table :wine_sizes
     create_table :wine_sizes do |t|
       t.string :title
       t.timestamps
--- db/migrate/20140312132130_categories_wine_sizes.rb
+++ db/migrate/20140312132130_categories_wine_sizes.rb
@@ -1,6 +1,5 @@
 class CategoriesWineSizes < ActiveRecord::Migration
   def self.up
-    drop_table :categories_wine_sizes
     create_table :categories_wine_sizes, :id => false do |t|
       t.integer :category_id
       t.integer :wine_size_id
