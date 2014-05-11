0a02f2d8b83da408a1d1a0646358fff1d425208d
Gaurav Sharma <gaurav2728@gmail.com>
2014-05-11 12:10:26 +0530

    remove unused column and form upate

--- app/models/contact_message.rb
+++ app/models/contact_message.rb
@@ -1,7 +1,6 @@
 class ContactMessage < ActiveRecord::Base
   validates_presence_of :header, :message => "is not blank?"
   validates_presence_of :message, :message => "is not blank?"
-  validates_format_of   :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   
   def show_errors
     return "- " + self.errors.full_messages.join("<br />- ")
