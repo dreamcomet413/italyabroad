db3dcc7f5304057bf69977c83ca63f05f62cedc2
Neeraj Kumar <neeraj.kumar@gmail.com>
2014-11-17 17:33:23 +0530

    some more changes in menus.rb migration file

--- db/migrate/20101203101312_create_menus.rb
+++ db/migrate/20101203101312_create_menus.rb
@@ -1,17 +1,19 @@
 class CreateMenus < ActiveRecord::Migration
   def self.up
-    #create_table :menus do |t|
-    #  t.integer :parent_id
-    #  t.integer :lft
-    #  t.integer :rgt
-    #  t.string :name
-    #  t.string :controller
-    #  t.string :action
-    #end
+    unless RAILS_ENV == "production"
+      create_table :menus do |t|
+        t.integer :parent_id
+        t.integer :lft
+        t.integer :rgt
+        t.string :name
+        t.string :controller
+        t.string :action
+      end
+    end
   end
 
   def self.down
-    remove_table :menus
+    remove_table :menus unless RAILS_ENV == "production"
   end
 end
 
