114eced80bdd53bbb4d4f6a7838412a64cec294b
Neeraj Kumar <neeraj.kumar@gmail.com>
2014-11-17 21:03:14 +0530

    check occasion id into products table

--- db/migrate/20101207115837_add_occasion_id_to_products.rb
+++ db/migrate/20101207115837_add_occasion_id_to_products.rb
@@ -1,7 +1,7 @@
 class AddOccasionIdToProducts < ActiveRecord::Migration
   def self.up
     #unless RAILS_ENV == "production"
-      add_column :products,:occasion_id,:integer,:default=>0
+      add_column :products,:occasion_id,:integer,:default=>0 unless column_exists?(:products, :occasion_id)
     #end
   end
 
