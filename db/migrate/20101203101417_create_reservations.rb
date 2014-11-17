939286d79707176421e8f74559a2a2c0c5e472ee
Neeraj Kumar <neeraj.kumar@gmail.com>
2014-11-17 17:40:55 +0530

    some changes in migrations

--- db/migrate/20101203101417_create_reservations.rb
+++ db/migrate/20101203101417_create_reservations.rb
@@ -1,16 +1,18 @@
 class CreateReservations < ActiveRecord::Migration
   def self.up
-     create_table :reservations do |t|
-       t.integer :restaurant_id
-       t.references :user
-       t.integer :status_reservation_id
-       t.timestamps
-       t.datetime :date
-     end
+    unless ActiveRecord::Base.connection.table_exists? :reservations
+      create_table :reservations do |t|
+        t.integer :restaurant_id
+        t.references :user
+        t.integer :status_reservation_id
+        t.timestamps
+        t.datetime :date
+      end
+    end
   end
 
   def self.down
-    remove_column :reservations
+    remove_column :reservations if ActiveRecord::Base.connection.table_exists? :reservations
   end
 end
 
--- db/migrate/20101203101520_create_status_reservations.rb
+++ db/migrate/20101203101520_create_status_reservations.rb
@@ -1,12 +1,14 @@
 class CreateStatusReservations < ActiveRecord::Migration
   def self.up
-    create_table :status_reservations do |t|
-      t.string :name
+    unless ActiveRecord::Base.connection.table_exists? :status_reservations
+      create_table :status_reservations do |t|
+        t.string :name
+      end
     end
   end
 
   def self.down
-    remove_table :status_reservations
+    remove_table :status_reservations if ActiveRecord::Base.connection.table_exists? :status_reservations
   end
 end
 
