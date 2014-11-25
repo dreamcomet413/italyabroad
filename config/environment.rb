71f67fdda30b292dcf7ddc225fd881313fc94d10
Gaurav Sharma <gaurav2728@gmail.com>
2014-11-25 13:46:45 +0530

    force utf8 encoding

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,6 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
+ItalyabroadNew::Application.initialize!
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
-ItalyabroadNew::Application.initialize!
-
