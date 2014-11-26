45843e966df09553cbe954e6b18ec2a3f78c3f05
Gaurav Sharma <gaurav2728@gmail.com>
2014-11-26 08:24:40 +0530

    show order items

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,7 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
-ItalyabroadNew::Application.initialize!
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
+ItalyabroadNew::Application.initialize!
+
