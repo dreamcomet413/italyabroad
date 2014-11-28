2b5a8880f46826e3ed3da8a1fb5262035a0482e0
Gaurav Sharma <gaurav2728@gmail.com>
2014-11-28 23:33:51 +0530

    show quantity when it have less

--- app/controllers/site/products_controller.rb
+++ app/controllers/site/products_controller.rb
@@ -19,7 +19,7 @@ class Site::ProductsController < ApplicationController
       @limited_stock =
         if @product.product_prices.empty?
           ""
-        elsif @product.quantity.to_i < 12
+        elsif quantity.to_i <= 24
           "Only #{quantity} left in stock."
         end
           #(@product.present? and @product.quantity.to_i < 12) ? "Only #{quantity} left in stock." : ""
