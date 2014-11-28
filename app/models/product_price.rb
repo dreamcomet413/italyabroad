c3bd52b232815295c57ba2f59fea955039279990
Gaurav Sharma <gaurav2728@gmail.com>
2014-11-28 09:33:06 +0530

    product quantity fix for single and multiple prices and quantity

--- app/models/product_price.rb
+++ app/models/product_price.rb
@@ -1,5 +1,5 @@
 class ProductPrice < ActiveRecord::Base
-  
+
   belongs_to :product
   validates_numericality_of :price
   validates_numericality_of :quantity
@@ -7,6 +7,10 @@ class ProductPrice < ActiveRecord::Base
 
   def show_errors
     return "- " + self.errors.full_messages.join("<br />- ")
+  end
+
+  def out_of_stock
+    self.quantity <= 0
   end
 
 end
