d55edd46a4a94beaeffd6f7cf33b726a2e6c9a40
Amrita Saha <iub.bba08@gmail.com>
2015-08-04 19:12:27 +0600

    cercavino custom back option developed to show the price options again

--- app/controllers/site/sommelier_controller.rb
+++ app/controllers/site/sommelier_controller.rb
@@ -3,7 +3,7 @@ class Site::SommelierController < ApplicationController
   layout "site"
 
   def index
-    session[:selected_search_options] = {}
+    session[:selected_search_options] = {}  unless params[:st].present?
     @questions = [
         "Which wine would you like to drink?",
         "Light, medium or full bodied?",
