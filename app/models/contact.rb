d172aa1b20bbb98109c10ef713dd5ed4555830d6
Amrita Saha <iub.bba08@gmail.com>
2015-08-19 00:48:40 +0600

    contact form solved

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,11 +1,11 @@
-class Contact < ActiveRecord::BaseWithoutTable
+class Contact < Tableless
   #apply_simple_captcha :message => " image and text were different", :add_to_base => true
   column :name, :string
   column :email, :string
   column :message, :text
   column :know_through, :string
   
-  validates_presence_of :name, :message => "is not blank?"
+  validates_presence_of :name, :message => "is blank"
   validates_format_of   :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   
   def show_errors
