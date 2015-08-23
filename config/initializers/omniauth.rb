0905e6296fbc8fc8d69d5281d571f1bf3b030903
Amrita Saha <iub.bba08@gmail.com>
2015-08-23 16:54:09 +0600

    new social media apps integrated

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'vLVmegFvgH1OqfHEi6kPYQ', 'dJHJdwafcZnGyvAUE5KWVJOrOLCSlNCcmcKCzpp3Qk', :access_token => '74390560-3bU2DSt3SM1ALA8kV97MjbZkJzYZHAvSiC2RZ7QhT'
-  provider :facebook, '631519600219374', '4a4324e295714c2026366374504f5ff4', :scope => 'user_birthday, user_location, offline_access,email'#,manage_pages'
+  provider :facebook, '899375176798298', '74361bf1694647233069a8c6195006d2', :scope => 'user_birthday, user_location, offline_access,email'#,manage_pages'
 end
