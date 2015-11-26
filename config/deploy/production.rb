199e0781e249e6148e462bacdf84c029ca3ee630
harshad <harshad@harshad-VirtualBox.(none)>
2015-11-26 18:14:18 +0530

    production email settings

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,11 +1,13 @@
-server "italyabroad.com", :app, :web, :db, :primary => true
-set :deploy_to, "/srv/italyabroad"
+server "52.26.183.145", :app, :web, :db, :primary => true
+set(:user) { "ubuntu" }
+set(:domain) { "52.26.183.145" }
+set :deploy_to, "/home/ubuntu/italyabroad"
 set :rake, 'bundle exec rake'
 
 #set :location, "italyabroad.com"
 #set :rails_env, 'production'
 
-#set :branch, 'master'
+set :branch, 'master'
 
 #role :app, '89.145.121.178'
 #role :web, '89.145.121.178'
--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,16 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
+require "smtp_ssl"
 
 # Initialize the rails application
 ItalyabroadNew::Application.initialize!
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
+
+ActionMailer::Base.smtp_settings = {
+:address => "smtp.gmail.com",
+:port => 587,
+:authentication => :plain,
+:user_name => "itserviceyouth@gmail.com",
+:password => "harshad@456"
+} 
