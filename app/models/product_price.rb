87b2da6a3ec38efcd8b1c0a3630d4db0c47d7ba9
Fahad Akhtar <fahad@goodlogics.com>
2016-01-19 13:55:00 +0500

    issue#4712 product creation, quantity and price issue

--- app/models/product_price.rb
+++ app/models/product_price.rb
@@ -3,7 +3,7 @@ class ProductPrice < ActiveRecord::Base
   belongs_to :product
   validates_numericality_of :price
   validates_numericality_of :quantity
-  validates_presence_of :product_id
+  # validates_presence_of :product
 
   def show_errors
     return "- " + self.errors.full_messages.join("<br />- ")
