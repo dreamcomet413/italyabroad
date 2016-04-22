d38a0be0ca2575e4fe1755f2351ab14e2c476911
hamza.khan <hamza.khan@goodlogics.com>
2016-04-22 12:18:37 +0500

    added migrattion of location field and added location on events landing pages

---
+++ db/migrate/20160421074620_add_location_field_to_products.rb
@@ -0,0 +1,9 @@
+class AddLocationFieldToProducts < ActiveRecord::Migration
+  def self.up
+    add_column :products, :location, :string
+  end
+
+  def self.down
+    remove_column :products, :location
+  end
+end
