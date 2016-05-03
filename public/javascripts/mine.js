da0b1c41d32e2f0b4f488e790ea76428e837e8ac
hamza.khan <hamza.khan@goodlogics.com>
2016-05-03 15:26:49 +0500

    added filters stripe functionality

--- public/javascripts/mine.js
+++ public/javascripts/mine.js
@@ -1,6 +1,18 @@
  $(document).ready(function(){
+ 	var filters = []	
   $('.field_text').click(function(){
  	$(this).parent().siblings('.field_value').val($(this).attr('value'));
+ 	var parent_name = $(this).parent().siblings('.field_value').attr('name');
+ 	filters = $('.selected-filters').find('.' + parent_name)
+ 	if (filters.length > 0) {
+ 		$('.selected-filters span.'+parent_name).html($(this).html());
+ 	}else{
+ 		$('.selected-filters').append("<div class='"+parent_name+"'></div>");
+ 		$('.selected-filters div.'+parent_name).append("<span class='"+ parent_name + "' >" + $(this).html() + "</span>" ) ;
+ 		$('.selected-filters div.'+parent_name).append("<span class='pull-right'><a href='#.' class='close-btn'>X</a></span> <br/>")
+  	}
+ 	$('.selected-filters').show();
+ 	
   });
   $('.submit_cart_form').click(function(){
   	
@@ -15,4 +27,12 @@
 	  });
 	});
 });
-
+ $(document).on('click', '.close-btn', function(){
+ 	parent_name = $('.close-btn').parent().parent().attr('class');
+	$('.panel-body').find('ul input#'+parent_name).attr('value', '')
+	$(this).parent().parent('div').remove();
+	if ($('.selected-filters').children().length == 0){
+		$('.selected-filters').hide();
+	}
+});
+  
