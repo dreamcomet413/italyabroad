3133f573beed1c43980e586c31abffdb9bd8a1da
hamza.khan <hamza.khan@goodlogics.com>
2016-05-05 19:26:42 +0500

    fixed search filters

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -53,110 +53,110 @@ class Site::SearchController < ApplicationController
     @searched_text = params[:text]
     
     action = false
+    if params[:text] != '' or params[:parent_type] or params[:region_id] or params[:producer_id]
+      if params[:wine_type].present? || params[:body_type].present? || params[:price_type].present? || params[:food_type].present?
+        @sort_by = available_sorting.include?(params[:sort_by]) ? params[:sort_by] : "product_prices.price asc"
+        @sort_by = 'products.name' if @sort_by.to_s.upcase == 'NAME'
 
-    if params[:wine_type].present? || params[:body_type].present? || params[:price_type].present? || params[:food_type].present?
-      @sort_by = available_sorting.include?(params[:sort_by]) ? params[:sort_by] : "product_prices.price asc"
-      @sort_by = 'products.name' if @sort_by.to_s.upcase == 'NAME'
+        if params[:price_type].present?
+          #Under £10	Between £10 and £20	More than £20
+          price_query_text = "? < 10" if params[:price_type] == "under 10"
+          price_query_text = "? > 10 AND ? < 20" if params[:price_type] == "between 10 and 20"
+          price_query_text = " ? > 20 " if params[:price_type] == "more than 20"
+          price_query_text= price_query_text.gsub("?","product_prices.price")
+        end
 
-      if params[:price_type].present?
-        #Under £10	Between £10 and £20	More than £20
-        price_query_text = "? < 10" if params[:price_type] == "under 10"
-        price_query_text = "? > 10 AND ? < 20" if params[:price_type] == "between 10 and 20"
-        price_query_text = " ? > 20 " if params[:price_type] == "more than 20"
-        price_query_text= price_query_text.gsub("?","product_prices.price")
+        if params[:price_start] and params[:price_end]
+          price_query_text = "? > #{params[:price_start]} AND ? < #{params[:price_end]}"
+        elsif params[:price_start] and params[:price_end]==""
+          price_query_text = " ? > #{params[:price_start]}"
+        elsif params[:price_start]=="" and params[:price_end]
+          price_query_text = "? < #{params[:price_end]}"
+        end
+
+        
+        @category =  Category.find_by_name(params[:wine_type]+"s")
+        @products = @category.present? ? @category.products : Product.where("surprise_me = ?",true)
+        @products = @products.where("products.body_type = ?",params[:body_type].to_s)  if params[:body_type].present?
+        @products = @products.where("products.with_food_type like ?","%#{params[:food_type].to_s}%") if params[:food_type].present?
+        @products = @products.joins(:product_prices).where(price_query_text) if params[:price_type].present?
+        @products = @products.where("active = ?",true).order(@sort_by)
+        @products = @products.paginate(:page => params[:page], :per_page => 10)
+      elsif params[:category] == "recipe"
+        @search = Search.new(params || {})
+        @recipes = Recipe.where(@search.conditions_for_recipes).paginate(:page => params[:page], :per_page => 10)
+
+        action = :recipes
+        
+      elsif params[:category] == "people"
+        #  if @searched_text.length > 3
+        @users = User.where("name LIKE ? AND city LIKE ?", "%#{params[:text]}%","%#{params[:city_text]}%").paginate(:page => params[:page], :per_page => 10)
+        #  end
+
+        action = :peoples
+
+      elsif params[:category] == "chef"
+        @type = Type.find(:first,:conditions=>['upper(name) = ?',params[:category].upcase])
+        #  if @searched_text.length > 3
+        @users = User.where("name LIKE ? AND type_id = ?", "%#{params[:text]}%",@type.id).paginate(:page => params[:page], :per_page => 10)
+        #  end
+
+        action = :peoples
+
+        #-------------
+      elsif params[:category] == "grape"
+        #@type = Type.find(:first,:conditions=>['upper(name) = ?',params[:category].upcase])
+        #  if @searched_text.length > 3
+        @users = Grape.where("name LIKE ? ", "%#{params[:text]}%").paginate(:page => params[:page], :per_page => 10)
+        #  end
+
+        action = :grapes
+
+        #-------------
+      elsif params[:category] == "all"
+        @search = Search.new(params || Hash.new)
+        @recipes = Recipe.find(:all, :conditions =>     @search.conditions_for_recipes)
+
+        @search = Search.new(params || Hash.new)
+
+        wine_conditions = ["upper(categories.name) LIKE ? AND products.name LIKE ? #{('AND moods.id = ' + params[:mood].strip) if params[:mood.present?]} AND products.active = ? AND product_prices.quantity > ? ",'WINE',"%#{params[:text].strip}%", true,0]
+        other_drinks_conditions = ["upper(categories.name) LIKE ? AND products.name LIKE ? #{('AND moods.id = ' + params[:mood].strip) if params[:mood.present?]} AND products.active = ? AND product_prices.quantity > ? ",'Other Drinks',"%#{params[:text].strip}%", true,0]
+        hamper_conditions = ["upper(categories.name) LIKE ? AND products.name LIKE ? #{('AND moods.id = ' + params[:mood].strip) if params[:mood.present?]} AND products.active = ? AND product_prices.quantity > ? ",'HAMPERS',"%#{params[:text].strip}%", true,0]
+        food_conditions = ["upper(categories.name) LIKE ? AND products.name LIKE ? #{('AND moods.id = ' + params[:mood].strip) if params[:mood.present?]} AND products.active = ? AND product_prices.quantity > ? ",'FOOD',"%#{params[:text].strip}%", true,0]
+        wine_events_conditions = ["upper(categories.name) LIKE ? AND products.name LIKE ? #{('AND moods.id = ' + params[:mood].strip) if params[:mood.present?]} AND products.active = ?  AND DATE(date) >= ?", 'EVENTS', "%#{params[:text].strip}%", true, Date.today]
+        @wines = Product.find(:all, :include => [:categories, :moods, :product_prices] ,:conditions => wine_conditions)
+        @other_drinks = Product.find(:all, :include => [:categories, :moods, :product_prices] ,:conditions => other_drinks_conditions)
+        @hampers = Product.find(:all, :include => [:categories, :moods, :product_prices] ,:conditions => hamper_conditions)
+        @foods = Product.find(:all, :include => [:categories, :moods, :product_prices] ,:conditions => food_conditions)
+
+        @wine_events = Product.find(:all, :include => [:categories, :moods, :product_prices] ,:conditions => wine_events_conditions)
+
+        SearchQuery.create(:query => @search.text) unless (@wines.blank? or @foods.blank?)
+
+        @users = User.find(:all, :conditions => ["name LIKE ?", "%#{params[:text]}%"])
+
+        @producers = Producer.find(:all, :conditions => ["name LIKE ? AND active = ?", "%#{params[:text]}%",true])
+
+        @grapes = Grape.find(:all, :conditions => ["name LIKE ? ", "%#{params[:text]}%"])
+
+        action = :all
+
+      else
+        #@sort_by = available_sorting.include?(params[:sort_by]) ? params[:sort_by] : "products.price DESC"
+
+        @sort_by = available_sorting.include?(params[:sort_by]) ? params[:sort_by] : "product_prices.price asc"
+        if @sort_by.to_s.upcase == 'NAME'
+          @sort_by = 'products.name'
+        end
+        @search = Search.new(params || Hash.new)
+        @category = @search.category
+        @products = Product.where(@search.conditions).includes([:categories, :grapes, :moods, :product_prices]).order(@sort_by).paginate(:page => params[:page], :per_page => 12)
+        SearchQuery.create(:query => @search.text) unless @products.blank?
+
+        action=false
+
       end
-
-      if params[:price_start] and params[:price_end]
-        price_query_text = "? > #{params[:price_start]} AND ? < #{params[:price_end]}"
-      elsif params[:price_start] and params[:price_end]==""
-        price_query_text = " ? > #{params[:price_start]}"
-      elsif params[:price_start]=="" and params[:price_end]
-        price_query_text = "? < #{params[:price_end]}"
-      end
-
-      
-      @category =  Category.find_by_name(params[:wine_type]+"s")
-      @products = @category.present? ? @category.products : Product.where("surprise_me = ?",true)
-      @products = @products.where("products.body_type = ?",params[:body_type].to_s)  if params[:body_type].present?
-      @products = @products.where("products.with_food_type like ?","%#{params[:food_type].to_s}%") if params[:food_type].present?
-      @products = @products.joins(:product_prices).where(price_query_text) if params[:price_type].present?
-      @products = @products.where("active = ?",true).order(@sort_by)
-      @products = @products.paginate(:page => params[:page], :per_page => 10)
-    elsif params[:category] == "recipe"
-      @search = Search.new(params || {})
-      @recipes = Recipe.where(@search.conditions_for_recipes).paginate(:page => params[:page], :per_page => 10)
-
-      action = :recipes
-      
-    elsif params[:category] == "people"
-      #  if @searched_text.length > 3
-      @users = User.where("name LIKE ? AND city LIKE ?", "%#{params[:text]}%","%#{params[:city_text]}%").paginate(:page => params[:page], :per_page => 10)
-      #  end
-
-      action = :peoples
-
-    elsif params[:category] == "chef"
-      @type = Type.find(:first,:conditions=>['upper(name) = ?',params[:category].upcase])
-      #  if @searched_text.length > 3
-      @users = User.where("name LIKE ? AND type_id = ?", "%#{params[:text]}%",@type.id).paginate(:page => params[:page], :per_page => 10)
-      #  end
-
-      action = :peoples
-
-      #-------------
-    elsif params[:category] == "grape"
-      #@type = Type.find(:first,:conditions=>['upper(name) = ?',params[:category].upcase])
-      #  if @searched_text.length > 3
-      @users = Grape.where("name LIKE ? ", "%#{params[:text]}%").paginate(:page => params[:page], :per_page => 10)
-      #  end
-
-      action = :grapes
-
-      #-------------
-    elsif params[:category] == "all"
-      @search = Search.new(params || Hash.new)
-      @recipes = Recipe.find(:all, :conditions =>     @search.conditions_for_recipes)
-
-      @search = Search.new(params || Hash.new)
-
-      wine_conditions = ["upper(categories.name) LIKE ? AND products.name LIKE ? #{('AND moods.id = ' + params[:mood].strip) if params[:mood.present?]} AND products.active = ? AND product_prices.quantity > ? ",'WINE',"%#{params[:text].strip}%", true,0]
-      other_drinks_conditions = ["upper(categories.name) LIKE ? AND products.name LIKE ? #{('AND moods.id = ' + params[:mood].strip) if params[:mood.present?]} AND products.active = ? AND product_prices.quantity > ? ",'Other Drinks',"%#{params[:text].strip}%", true,0]
-      hamper_conditions = ["upper(categories.name) LIKE ? AND products.name LIKE ? #{('AND moods.id = ' + params[:mood].strip) if params[:mood.present?]} AND products.active = ? AND product_prices.quantity > ? ",'HAMPERS',"%#{params[:text].strip}%", true,0]
-      food_conditions = ["upper(categories.name) LIKE ? AND products.name LIKE ? #{('AND moods.id = ' + params[:mood].strip) if params[:mood.present?]} AND products.active = ? AND product_prices.quantity > ? ",'FOOD',"%#{params[:text].strip}%", true,0]
-      wine_events_conditions = ["upper(categories.name) LIKE ? AND products.name LIKE ? #{('AND moods.id = ' + params[:mood].strip) if params[:mood.present?]} AND products.active = ?  AND DATE(date) >= ?", 'EVENTS', "%#{params[:text].strip}%", true, Date.today]
-      @wines = Product.find(:all, :include => [:categories, :moods, :product_prices] ,:conditions => wine_conditions)
-      @other_drinks = Product.find(:all, :include => [:categories, :moods, :product_prices] ,:conditions => other_drinks_conditions)
-      @hampers = Product.find(:all, :include => [:categories, :moods, :product_prices] ,:conditions => hamper_conditions)
-      @foods = Product.find(:all, :include => [:categories, :moods, :product_prices] ,:conditions => food_conditions)
-
-      @wine_events = Product.find(:all, :include => [:categories, :moods, :product_prices] ,:conditions => wine_events_conditions)
-
-      SearchQuery.create(:query => @search.text) unless (@wines.blank? or @foods.blank?)
-
-      @users = User.find(:all, :conditions => ["name LIKE ?", "%#{params[:text]}%"])
-
-      @producers = Producer.find(:all, :conditions => ["name LIKE ? AND active = ?", "%#{params[:text]}%",true])
-
-      @grapes = Grape.find(:all, :conditions => ["name LIKE ? ", "%#{params[:text]}%"])
-
-      action = :all
-
-    else
-      #@sort_by = available_sorting.include?(params[:sort_by]) ? params[:sort_by] : "products.price DESC"
-
-      @sort_by = available_sorting.include?(params[:sort_by]) ? params[:sort_by] : "product_prices.price asc"
-      if @sort_by.to_s.upcase == 'NAME'
-        @sort_by = 'products.name'
-      end
-      @search = Search.new(params || Hash.new)
-      @category = @search.category
-      @products = Product.where(@search.conditions).includes([:categories, :grapes, :moods, :product_prices]).order(@sort_by).paginate(:page => params[:page], :per_page => 10)
-      SearchQuery.create(:query => @search.text) unless @products.blank?
-
-      action=false
-
-    end
-
+    end    
     respond_to do |format|
         format.html { 
           render :action => action if action 
