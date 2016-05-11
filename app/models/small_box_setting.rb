58453ae46b8887e84577f3c42bade98bff2b7a9c
Fahad Akhtar <fahad@goodlogics.com>
2016-05-11 18:52:07 +0500

    Fixed small box image upload issue

--- app/models/small_box_setting.rb
+++ app/models/small_box_setting.rb
@@ -1,4 +1,6 @@
 class SmallBoxSetting < ActiveRecord::Base
 	belongs_to :image, :class_name => "Image", :foreign_key => "image_id",:dependent=>:destroy
 	validates :box_title , uniqueness: true
+
+	accepts_nested_attributes_for :image #, reject_if: proc { |attributes| attributes['image_filename'].blank? }
 end
