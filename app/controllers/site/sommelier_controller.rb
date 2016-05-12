ea26edcadab20d9f0a6222438bb5832629b6753c
hamza.khan <hamza.khan@goodlogics.com>
2016-05-12 16:13:39 +0500

    fixed 500 error issue

--- app/controllers/site/sommelier_controller.rb
+++ app/controllers/site/sommelier_controller.rb
@@ -14,6 +14,9 @@ class Site::SommelierController < ApplicationController
   end
 
   def create
+
+    session[:selected_search_options] ||= {}
+
     @questions = [
         "Which wine would you like to drink?",
         "Light, medium or full bodied?",
@@ -21,32 +24,35 @@ class Site::SommelierController < ApplicationController
         "How much would you like to spend?",
         "What type of food is the wine for?"
     ]
+
     session[:selected_search_options][:wine_type] = params[:selected_option] if ["Red Wine", "Rose Wine", "White Wine", "Sparkling Wine", "Surprise Me"].include?(params[:selected_option])
     session[:selected_search_options][:body_type] = params[:selected_option] if ["Light", "Medium", "Full Body"].include?(params[:selected_option])
     session[:selected_search_options][:price_type] = params[:selected_option].to_s.gsub("£", "") if ["under £10", "between £10 and £20", "more than £20"].include?(params[:selected_option])
     session[:selected_search_options][:food_type] = params[:selected_option] if ["red meat", "white meat", "pasta", "fish", "cheeses", "desserts"].include?(params[:selected_option])
     session[:selected_search_options][:choice] = params[:selected_option] if ["drink on its own","with food"].include?(params[:selected_option])
 
-     if params[:selected_option] == "Sparkling Wine"
-       params[:step] = "2"
-     end
+    cookies[:selected_wine] = session[:selected_search_options][:wine_type] if ["Red Wine", "Rose Wine", "White Wine", "Sparkling Wine", "Surprise Me"].include?(params[:selected_option])
+    logger.info(cookies[:selected_wine])  
+    logger.info('===============================================================')
 
+    if params[:selected_option] == "Sparkling Wine"
+      params[:step] = "2"
+    end
     search_options =
-        case params[:step]
-          when "1"
-            ["Light", "Medium", "Full Body"]
-          when "2"
-            ["drink on its own", "with food"]
-          when "3"
-            if params[:selected_option] == "with food"
-              ["red meat", "white meat", "pasta", "fish", "cheeses", "desserts"]
-            else
-              ["under £10", "between £10 and £20", "more than £20"]
-            end
-          when "4"
-            ["under £10", "between £10 and £20", "more than £20"] if ["red meat", "white meat", "pasta", "fish", "cheeses", "desserts"].include?(params[:selected_option])
-        end
-
+    case params[:step]
+    when "1"
+      ["Light", "Medium", "Full Body"]
+    when "2"
+      ["drink on its own", "with food"]
+    when "3"
+      if params[:selected_option] == "with food"
+        ["red meat", "white meat", "pasta", "fish", "cheeses", "desserts"]
+      else
+        ["under £10", "between £10 and £20", "more than £20"]
+      end
+    when "4"
+      ["under £10", "between £10 and £20", "more than £20"] if ["red meat", "white meat", "pasta", "fish", "cheeses", "desserts"].include?(params[:selected_option])
+    end
     respond_to do |format|
       format.html {}
       format.js {
@@ -60,10 +66,10 @@ class Site::SommelierController < ApplicationController
             page[".items"].html("")
             page[".items"].html(render :partial => "search_options", :locals => {:item_options => search_options, :step => (params[:step].to_i + 1).to_s})
           else
-            page.redirect_to site_search_index_url(session[:selected_search_options])
+            page.redirect_to site_search_index_path(price_type: session[:selected_search_options][:price_type] , wine_type: cookies[:selected_wine] , parent_type: 'wine')
           end
         end
       }
     end
   end
-end
+end 
