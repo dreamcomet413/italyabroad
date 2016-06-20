c5d7024f25e241949333a784fec4555b1a5bf6d8
Fahad Akhtar <fahad@goodlogics.com>
2016-06-20 11:33:26 +0500

    Fixed issues reported by Andrea

--- app/models/wine_list.rb
+++ app/models/wine_list.rb
@@ -2,8 +2,10 @@ class WineList < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
   
-  def initialize(params={})
-    super
+  before_save :set_quantity
+  
+  def set_quantity
+    # super
     self.quantity = 1 if self.quantity.blank?
   end
 end
