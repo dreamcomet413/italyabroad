29b9cb87ca7dc40b2a918d52362004ed26626a61
Fahad Akhtar <fahad@goodlogics.com>
2016-08-23 14:09:44 +0500

    added meta info to show pages

--- app/models/grape.rb
+++ app/models/grape.rb
@@ -5,5 +5,28 @@ class Grape < ActiveRecord::Base
   has_many :products, :through => :products_grapes
 
   friendly_identifier :name
+
+  def page_title_formatted
+    page_title.blank? ? name : page_title
+  end
+
+  def meta_description_formatted
+    meta_description.blank? ? description[0..500] : meta_description
+  end
+
+  def meta_keys_formatted
+    if meta_keys.blank?
+      keys = []
+      i = 0
+      for key in description.downcase.split
+        i += 1
+        keys << key.to_s.downcase.gsub(/[^a-z0-9]+/, '-').gsub(/-+$/, '').gsub(/^-+$/, '') unless keys.include?(key) || i > 21 || key.length < 4
+      end
+      return keys.join(", ")
+    else
+      return meta_keys
+    end
+  end
+
 end
 
