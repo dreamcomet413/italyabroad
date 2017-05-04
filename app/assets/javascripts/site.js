7763397120d08de0597efefd31530a4ace33b184
Muhammad ALi <muhammad.ali@goodlogics.com>
2017-05-04 10:50:09 +0500

    New message Alert

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -8,3 +8,10 @@
 //= require swiper
 //= require mine
 //= require custom
+
+function popupwindow(url, w, h) {
+  title = "Live Chat"
+  var left = (screen.width/2)-(w/2);
+  var top = (screen.height/2)-(h/2);
+  return window.open(url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+} 
