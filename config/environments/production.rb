c653a65b9c61b9fbabb4e25f0fed335cb82c2290
rakesh kumar <rakeshmaurya262@gmail.com>
2017-07-23 17:34:20 +0530

    url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,7 @@ ItalyabroadNew::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   # config.action_mailer.perform_deliveries = true # need to change
+  config.action_mailer.default_url_options = { host: 'https://www.italyabroad.com'}
   config.action_mailer.delivery_method = :smtp 
 
   config.action_mailer.smtp_settings = {
